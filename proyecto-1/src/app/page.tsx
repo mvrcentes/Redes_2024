a1614a2fd46c7dbb149d2ffe9c4b404a363184b6
mvrcentes <mvrcentes@hotmail.com>
2024-08-06 10:56:04 -0600

    Add: add context app

--- proyecto-1/src/app/page.tsx
+++ proyecto-1/src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Signin } from "./components/auth/Signin/Signin"
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#202022]">
