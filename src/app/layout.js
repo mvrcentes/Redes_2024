ce580622999e5c5030d5c7f8f204b07d72a56962
mvrcentes <mvrcentes@hotmail.com>
2024-08-24 17:17:14 -0600

    Add: add toast

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { Toaster } from "@/components/ui/toaster"
 import { XMPPProvider } from "@/context/xmppContext"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <XMPPProvider>{children}</XMPPProvider>
+        <Toaster />
       </body>
     </html>
   )
